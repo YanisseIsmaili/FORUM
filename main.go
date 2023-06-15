d4a3afbaf3750c98e98e1114f933a4262d312845
Zephyr <nils.jaudon@ynov.com>
2023-06-15 10:53:35 +0200

    ConfirmPassword

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ func main() {
 	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
 		log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
 	}
-	r.StaticFile("/styles.css", "./Frontend/styles.css")
-	r.LoadHTMLGlob("Frontend/*")
-	r.GET("/loging", func(c *gin.Context) { // récupére le formulaire : 
+	r.StaticFile("/styles.css", "./Frontend/styles.css") // permet de link le fichier css
+	r.LoadHTMLGlob("Frontend/*") // permet d'allez chercher
+	r.GET("/loging", func(c *gin.Context) { 
 		c.HTML(http.StatusOK, "log&Signup.html", gin.H{
 			"title": "Main website",
 		})
 	})
+
 	r.Run()
 }
