71a5f6d592db93fd78d6348c979d9e1227c27703
M1ck3y-ru <clement.garciaspro@gmail.com>
2023-06-22 10:29:30 +0200

    databsa

--- Backend/database.go
+++ Backend/database.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+	"Forum/Backend/Config"
+	"MySql"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/jinzhu/gorm"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -40,41 +55,13 @@ type Comments struct {
 // Fonction pour créer et initialiser la base de données
 func CreateDB() {
 	// Connexion à la base de données
-<<<<<<< HEAD
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "Forum:1234@tcp(localhost:3306)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Erreur de connexion à la base de données:", err)
 	}
-}
-
-// Fonction pour créer les tables
-func CreateTables() {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
-=======
-	dsn := "Forum:1234@tcp(:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
->>>>>>> 70a9d312c3d0995081ed54b777ad7864dc2b592e
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Erreur de connexion à la base de données:", err)
-	}
-
 	// Création des tables
 	db.AutoMigrate(&Users{}, &Posts{}, &Comments{})
-}
-
-// Fonction pour supprimer les tables
-func DropTables() {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Erreur de connexion à la base de données:", err)
-	}
-
-	// Suppression des tables
-	db.Migrator().DropTable(&Users{}, &Posts{}, &Comments{})
 }
 
 // Fonction pour ajouter un utilisateur
@@ -162,182 +149,157 @@ func ShowComments() {
 }
 
 // Fonction pour afficher un utilisateur
-func ShowUser(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// func ShowUser(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Affichage d'un utilisateur
-	var user Users
-	db.First(&user, id)
-	fmt.Println(user)
-}
+// 	// Affichage d'un utilisateur
+// 	var user Users
+// 	db.First(&user, id)
+// 	fmt.Println(user)
+// }
 
-// Fonction pour afficher un post
-func ShowPost(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
+// // Fonction pour afficher un post
+// func ShowPost(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Affichage d'un post
-	var post Posts
-	db.First(&post, id)
-	fmt.Println(post)
-}
+// 	// Affichage d'un post
+// 	var post Posts
+// 	db.First(&post, id)
+// 	fmt.Println(post)
+// }
 
 // Fonction pour afficher un commentaire
-func ShowComment(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
+// func ShowComment(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Affichage d'un commentaire
-	var comment Comments
-	db.First(&comment, id)
-	fmt.Println(comment)
-}
+// 	// Affichage d'un commentaire
+// 	var comment Comments
+// 	db.First(&comment, id)
+// 	fmt.Println(comment)
+// }
 
-// Fonction pour modifier un utilisateur
-func UpdateUser(id uint, username string, email string, password string) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour modifier un utilisateur
+// func UpdateUser(id uint, username string, email string, password string) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Modification de l'utilisateur
-	var user Users
-	db.First(&user, id)
-	user.Username = username
-	user.Email = email
-	user.Password = password
-	db.Save(&user)
-}
+// 	// Modification de l'utilisateur
+// 	var user Users
+// 	db.First(&user, id)
+// 	user.Username = username
+// 	user.Email = email
+// 	user.Password = password
+// 	db.Save(&user)
+// }
 
-// Fonction pour modifier un post
-func UpdatePost(id uint, title string, content string, userID uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour modifier un post
+// func UpdatePost(id uint, title string, content string, userID uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Modification du post
-	var post Posts
-	db.First(&post, id)
-	post.Title = title
-	post.Content = content
-	post.UserID = userID
-	db.Save(&post)
-}
+// 	// Modification du post
+// 	var post Posts
+// 	db.First(&post, id)
+// 	post.Title = title
+// 	post.Content = content
+// 	post.UserID = userID
+// 	db.Save(&post)
+// }
 
-// Fonction pour modifier un commentaire	// Connexion à la base de données
-func UpdateComment(id uint, content string, userID uint, postID uint) {
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour modifier un commentaire	// Connexion à la base de données
+// func UpdateComment(id uint, content string, userID uint, postID uint) {
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Modification du commentaire
-	var comment Comments
-	db.First(&comment, id)
-	comment.Content = content
-	comment.UserID = userID
-	comment.PostID = postID
-	db.Save(&comment)
-}
+// 	// Modification du commentaire
+// 	var comment Comments
+// 	db.First(&comment, id)
+// 	comment.Content = content
+// 	comment.UserID = userID
+// 	comment.PostID = postID
+// 	db.Save(&comment)
+// }
 
-// Fonction pour supprimer un utilisateur
-func DeleteUser(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour supprimer un utilisateur
+// func DeleteUser(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression de l'utilisateur
-	var user Users
-	db.First(&user, id)
-	db.Delete(&user)
-}
+// 	// Suppression de l'utilisateur
+// 	var user Users
+// 	db.First(&user, id)
+// 	db.Delete(&user)
+// }
 
-// Fonction pour supprimer un post
-func DeletePost(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour supprimer un post
+// func DeletePost(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression du post
-	var post Posts
-	db.First(&post, id)
-	db.Delete(&post)
-}
+// 	// Suppression du post
+// 	var post Posts
+// 	db.First(&post, id)
+// 	db.Delete(&post)
+// }
 
-// Fonction pour supprimer un commentaire
-func DeleteComment(id uint) {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour supprimer un commentaire
+// func DeleteComment(id uint) {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression du commentaire
-	var comment Comments
-	db.First(&comment, id)
-	db.Delete(&comment)
-}
+// 	// Suppression du commentaire
+// 	var comment Comments
+// 	db.First(&comment, id)
+// 	db.Delete(&comment)
+// }
 
-// Fonction pour supprimer tous les utilisateurs
-func DeleteAllUsers() {
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// // Fonction pour supprimer tous les utilisateurs
+// func DeleteAllUsers() {
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression de tous les utilisateurs
-	var users []Users
-	db.Find(&users)
-	db.Delete(&users)
-}
+// 	// Suppression de tous les utilisateurs
+// 	var users []Users
+// 	db.Find(&users)
+// 	db.Delete(&users)
+// }
 
-// Fonction pour supprimer tous les posts
-func DeleteAllPosts() {
+// // Fonction pour supprimer tous les posts
+// func DeleteAllPosts() {
 
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression de tous les posts
-	var posts []Posts
-	db.Find(&posts)
-	db.Delete(&posts)
-}
+// 	// Suppression de tous les posts
+// 	var posts []Posts
+// 	db.Find(&posts)
+// 	db.Delete(&posts)
+// }
 
-// Fonction pour supprimer tous les commentaires
-func DeleteAllComments() {
+// // Fonction pour supprimer tous les commentaires
+// func DeleteAllComments() {
 	
-	// Connexion à la base de données
-	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	// Connexion à la base de données
+// 	dsn := "Forum:1234@tcp(localhost:8080)/NerdzMethology?parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// Suppression de tous les commentaires
-	var comments []Comments
-	db.Find(&comments)
-	db.Delete(&comments)
-}
-
-// Fonction pour supprimer la base de données
-func DeleteDB() {
-	
-	// Suppression de la base de données
-	os.Remove("NerdzMethology")
-}
-
-// Fonction pour supprimer toutes les tables
-func DeleteAllTables() {
-	
-	// Suppression de toutes les tables
-	os.Remove("Users")
-	os.Remove("Posts")
-	os.Remove("Comments")
-}
-
-// Fonction pour supprimer toutes les données
-func DeleteAllData() {
-	
-	// Suppression de toutes les données
-	os.Remove("Users")
-	os.Remove("Posts")
-	os.Remove("Comments")
-}
+// 	// Suppression de tous les commentaires
+// 	var comments []Comments
+// 	db.Find(&comments)
+// 	db.Delete(&comments)
+// }
