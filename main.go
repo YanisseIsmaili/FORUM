3cb65eafce7419872ed013354dada1eecf68be02
Zephyr <nils.jaudon@ynov.com>
2023-06-26 20:09:26 +0200

    AllGoodNowRegister

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Forum/Backend/controller"
 	db "Forum/Backend/database"
+
 	"Forum/Backend/service"
 	"fmt"
 	"log"
@@ -12,6 +13,8 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+const IndexURL = "/index"
 
 func main() {
 	dsn := "NerdMythology.db"
@@ -29,6 +32,7 @@ func main() {
 	}
 	r.StaticFile("/styles.css", "./Frontend/styles.css") // permet de link le fichier css
 	r.LoadHTMLGlob("Frontend/*")                         // permet d'allez chercher
+
 	r.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "log&Signup.html", gin.H{})
 	})
@@ -39,14 +43,34 @@ func main() {
 		var loginController controller.LoginController = controller.LoginHandler(loginService, jwtService)
 		token := loginController.Login(ctx)
 		if token != "" {
-			fmt.Println("token  found")
-			ctx.JSON(http.StatusOK, gin.H{
-				"token": token,
-			})
+			fmt.Println("Token found")
+			ctx.SetCookie("token", token, 3600, "/", "", false, true)
+			ctx.Redirect(http.StatusFound, "/index")
+			return
 		} else {
-			ctx.JSON(http.StatusUnauthorized, nil)
-			fmt.Println("token not found")
+			fmt.Println("Token not found")
+			ctx.Redirect(http.StatusFound, "/login")
+			return
 		}
 	})
+
+	// Middleware pour vérifier le token à chaque requête
+	r.Use(func(c *gin.Context) {
+		token, err := c.Cookie("token")
+		if err != nil || token == "" {
+			// Rediriger vers la page de connexion si le token est invalide ou non présent
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+			return
+		}
+		c.Next()
+	})
+
+	r.GET("/index", func(c *gin.Context) {
+		// Code pour gérer l'accès à la page /index lorsque le token est valide
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	
 	r.Run(":8089")
 }
