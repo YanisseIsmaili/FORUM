532b0cd44abe6c53775f178d92ab6169c7f3dba4
Zephyr <nils.jaudon@ynov.com>
2023-06-27 17:08:43 +0200

    TestBdd

--- Backend/service/LoginService.go
+++ Backend/service/LoginService.go
@@ -34,12 +34,12 @@ func (info *loginInformation) LoginUser(email string, password string) bool {
 		Email:    info.emailUser,
 		Password: info.passwordUser,
 	}
-	
+
 	if err := info.db.Where("email = ? AND password = ?", user.Email, user.Password).First(&user).Error; err != nil {
 		fmt.Println("User not found")
 		return false
 	}
-	
+
 	// Vérification du mot de passe
 	return info.emailUser == email && info.passwordUser == password
 
