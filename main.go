b2a6b16ed32b1050e1a20b2ed427272deaca70a4
Zephyr <nils.jaudon@ynov.com>
2023-06-27 19:45:25 +0200

    Register&LoginGood

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	database "Forum/Backend/Database"
-	service "Forum/Backend/Service"
+	"fmt"
 	"log"
 	"net/http"
+
+	database "Forum/Backend/Database"
+	service "Forum/Backend/Service"
+	"Forum/Backend/controller"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
@@ -39,19 +42,48 @@ func main() {
 
 	// permet de vérifier les informatinos du méthod post
 	r.POST("/login", func(ctx *gin.Context) {
-
+		loginService := service.StaticLoginService(ctx, dbConnector)
+		var jwtService service.JWTService = service.JWTAuthService()
+		var loginController controller.LoginController = controller.LoginHandler(loginService, jwtService)
+		token := loginController.Login(ctx)
+		if token != "" {
+			fmt.Println("Token found")
+			ctx.SetCookie("token", token, 3600, "/", "", false, true)
+			ctx.Redirect(http.StatusFound, "/index")
+			return
+		} else {
+			fmt.Println("Token not found")
+			ctx.Redirect(http.StatusFound, "/login")
+			return
+		}
 	})
 
-	r.GET("/logout", func(c *gin.Context) {
-		c.Redirect(http.StatusFound, "/login")
-	})
 	r.POST("/login/register", func(c *gin.Context) {
-
 		service.RegisterUser(c, dbConnector)
 		c.Redirect(http.StatusFound, "/login")
-
 	})
 
-	r.Run(":8089")
+	r.GET("/createPost", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "Post.html", gin.H{})
+	})
 
+	r.Use(func(c *gin.Context) {
+		token, err := c.Cookie("token")
+		if err != nil || token == "" {
+			// Rediriger vers la page de connexion si le token est invalide ou non présent
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+			return
+		}
+		c.Next()
+	})
+
+	r.GET("/index", func(c *gin.Context) {
+		// Code pour gérer l'accès à la page /index lorsque le token est valide
+
+		// Code pour gérer l'accès à la page /index lorsque le token est valide
+
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	r.Run(":8089")
 }
